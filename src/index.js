1f4edfec20265ca2edb5a19b8080f8f5907121b1
WhitigolProd <36452182+WhitigolProd@users.noreply.github.com>
2022-06-04 15:08:36 -0400

    Updated Icons and Size

--- src/index.js
+++ src/index.js
@@ -10,10 +10,13 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 645,
+    height: 500,
+    minWidth: 645,
+    minHeight: 500,
     transparent: true,
     titleBarStyle: 'hidden',
+    icon: path.join(__dirname, 'img/icon.png'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
