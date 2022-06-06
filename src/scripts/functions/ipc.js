cc5e1928535d3b46b3e5b7a9aee817fc07a033b7
Casper <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-06-06 17:34:07 +0200

    :bug: fix: set correct event handler for close btn

--- src/scripts/functions/ipc.js
+++ src/scripts/functions/ipc.js
@@ -1,6 +1,10 @@
 const { ipcRenderer } = require("electron")
 const ipc = ipcRenderer;
 
-elements.titlebar.buttons.close.on('click', ipc.send('close-app'))
+elements.titlebar.buttons.close.on('click', () => {
+  ipc.send('close-app')
+})
 
-elements.titlebar.buttons.minimize.on('click', ipc.send('close-app'))
+elements.titlebar.buttons.minimize.on('click', () => {
+  ipc.send('close-app')
+})
