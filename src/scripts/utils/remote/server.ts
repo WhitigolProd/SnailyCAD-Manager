ad67a24cfcd93b07609c7b014872ab3af9f37496
WhitigolProd <36452182+WhitigolProd@users.noreply.github.com>
2022-10-09 19:14:05 -0400

    🚀 — Create Express Boilerplate for Remote Server

---
+++ src/scripts/utils/remote/server.ts
@@ -0,0 +1,11 @@
+// @ts-nocheck beacuse typescript doesn't seem to know how tf express works :)
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "SnailyCAD Manager Remote Server Boilerplate Created!",
+  });
+});
