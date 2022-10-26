944d104688ac974c6e92edb63ee87eec47f3f45f
WhitigolProd <36452182+WhitigolProd@users.noreply.github.com>
2022-10-25 22:39:27 -0400

    🔨 (ENV) — Added Open/Close Functions
    🔨 (ENV) — Added Open/Close Functions
    🔨 (ENV) — Created Save Base

--- src/scripts/utils/env.ts
+++ src/scripts/utils/env.ts
@@ -24,6 +24,7 @@ const envClass = class {
             | 'DISCORD_CLIENT_ID'
             | 'DISCORD_CLIENT_SECRET'
             | 'STEAM_API_KEY'
+            | '__empty__'
     ) {
         this.selector = selector;
     }
@@ -103,8 +104,34 @@ const envClass = class {
         return null;
     }
 
-    write() {
-        fs.writeFile();
+    save() {
+        let parsenv = require('parsenv');
+
+        parsenv.edit({
+            POSTGRES_PASSWORD: `${$('#env_POSTGRES_PASSWORD').val()}`,
+            POSTGRES_USER: `${$('#env_POSTGRES_USER').val()}`,
+            DB_HOST: `${$('#env_DB_HOST').val()}`,
+            DB_PORT: `${$('#env_PORT').val()}`,
+            POSTGRES_DB: `${$('#env_POSTGRES_DB').val()}`,
+            JWT_SECRET: `${$('#env_JWT_SECRET').val()}`,
+            ENCRYPTION_TOKEN: `${$('#env_ENCRYPTION_TOKEN').val()}`,
+            CORS_ORIGIN_URL: `${$('#env_CORS_ORIGIN_URL').val()}`,
+            NEXT_PUBLIC_PROD_ORIGIN: `${$(
+                '#env_NEXT_PUBLIC_PROD_ORIGIN'
+            ).val()}`,
+            DOMAIN: `${$('#env_DOMAIN').val()}`,
+            PORT_CLIENT: `${$('#env_PORT_CLIENT').val()}`,
+            PORT_API: `${$('#env_PORT_API').val()}`,
+            DISCORD_BOT_TOKEN: `${$('#env_DISCORD_BOT_TOKEN').val()}`,
+            DISCORD_SERVER_ID: `${$('#env_DISCORD_SERVER_ID').val()}`,
+            DISCORD_CLIENT_ID: `${$('#env_DISCORD_CLIENT_ID').val()}`,
+            DISCORD_CLIENT_SECRET: `${$('#env_DISCORD_CLIENT_SECRET').val()}`,
+            STEAM_API_KEY: `${$('#env_STEAM_API_KEY').val()}`,
+            SECURE_COOKIES_FOR_IFRAME: `${$(
+                '#env_SECURE_COOKIES_FOR_IFRAME'
+            ).prop('checked')}`,
+            TELEME: `${$('#env_POSTGRES_PASSWORD').val()}`,
+        });
     }
 };
 
@@ -154,19 +181,82 @@ const createEnvInputs = () => {
         'DISCORD_CLIENT_ID',
         'DISCORD_CLIENT_SECRET',
         'STEAM_API_KEY',
+        'SECURE_COOKIES_FOR_IFRAME',
+        'TELEMETRY_ENABLED',
     ];
 
     fields.forEach((field) => {
+        if (field === 'SECURE_COOKIES_FOR_IFRAME') {
+            $('#env_content').append(`
+            <label for="env_SECURE_COOKIES_FOR_IFRAME">
+                <input type="checkbox" id="env_SECURE_COOKIES_FOR_IFRAME" name="SEUCRE_COOKIES_FOR_IFRAME" role="switch">
+                Secure Cookies for iframe
+            </label>
+            `);
+            return;
+        }
+
+        if (field === 'TELEMETRY_ENABLED') {
+            $('#env_content').append(`
+            <label for="env_TELEMETRY_ENABLED">
+                <input type="checkbox" id="env_TELEMETRY_ENABLED" name="TELEMETRY_ENABLED" role="switch">
+                Telemetry Enabled
+            </label>
+            `);
+            return;
+        }
+
         $('#env_content').append(`
         <label for="env_${field}">
-        ${field}
+        ${field.replaceAll('_', ' ')}
             <input
                 type="text"
                 id="env_${field}"
-                placeholder="${field}"
+                placeholder="${field.replaceAll('_', ' ')}"
                 name="${field}"
             />
         </label>
         `);
+    });
+};
+
+const loadEnvValues = () => {
+    let fields = [
+        'POSTGRES_PASSWORD',
+        'POSTGRES_USER',
+        'DB_HOST',
+        'DB_PORT',
+        'POSTGRES_DB',
+        'JWT_SECRET',
+        'ENCRYPTION_TOKEN',
+        'CORS_ORIGIN_URL',
+        'NEXT_PUBLIC_PROD_ORIGIN',
+        'DOMAIN',
+        'PORT_CLIENT',
+        'PORT_API',
+        'DISCORD_BOT_TOKEN',
+        'DISCORD_SERVER_ID',
+        'DISCORD_CLIENT_ID',
+        'DISCORD_CLIENT_SECRET',
+        'STEAM_API_KEY',
+        'SECURE_COOKIES_FOR_IFRAME',
+        'TELEMETRY_ENABLED',
+    ];
+
+    fields.forEach((field) => {
+        if (
+            field === 'SECURE_COOKIES_FOR_IFRAME' ||
+            field === 'TELEMETRY_ENABLED'
+        ) {
+            if (env(field).read() == 'true') {
+                $(`#env_${field}`).prop('checked', true);
+            } else {
+                $(`#env_${field}`).prop('checked', false);
+            }
+            return;
+        }
+
+        // @ts-expect-error
+        $(`#env_${field}`).val(`${env(`${field}`).read() || ''}`);
     });
 };
